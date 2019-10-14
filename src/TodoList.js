df3d5aaaeba8cefc5fc0e179114f93796f9b2d35
DGiananris <mtsgiannaris@gmail.com>
2019-10-14 12:30:15 +0300

    added save/load items via localStorage.todo delete/complete item

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,44 +1,47 @@
-import React from 'react';
-
+import React,{useEffect} from 'react';
+import {useRef} from 'react';
 import './TodoList.css';
 
 function TodoList(props) {
- //let loadedtodos=[];
+ let loadedtodos;
+ let trimmedtodos;
+
+const todosRef = useRef(props.todos);
+todosRef.current=props.todos;
+
+ const itemsGet=(key)=>{
+
+   loadedtodos={...localStorage}
+   trimmedtodos=Object.values(loadedtodos).map((item)=>
+      item=item.match(/\w/ig).join('')
+   )
+ }
 
 
-  //props.todos.map((i,ind) =>{
-  //   if(i!==''){
-  //     // return(
-  //     //   <li className="item" key={i+ind.toString()}>
-  //     //     <div className="item-box">{i}</div>
-  //     //   </li> )
-  //
-  //   }});
+ const itemsSave =(key)=>{
+   let str = JSON.stringify(props.todos);
+   localStorage.setItem('todos'+key,str);
+ }
+
+ useEffect(() => {
+     const items = setTimeout(() => {
+       props.todos.map((i,ind) =>{
+         //console.log(i)
+
+       if(i!==''){
+         itemsSave(i);
+
+       }
+     });
+   }, 2000);
+
+     return () => clearTimeout(items);//what do we say to the god of memory leaks? not today.
+   }, [props.todos]);
 
 
- // const itemsSave =(key)=>{
- //   let str = JSON.stringify(props.todos);
- //   localStorage.setItem('todos'+key,str);
- // }
- //
- //
- // const itemsGet=(key)=>{
- //   let str =localStorage.getItem('todos'+key);
- //   loadedtodos=JSON.parse(str);
- // }
- //
- // const items=props.todos.map((i,ind) =>{
- //
- //   if(i!==''){
- //     itemsSave(i);
- //     itemsGet(i);
- //   }
- // });
- //
- // console.log(localStorage);
-
-
-const itemsShow= props.todos.map((i,ind)=>{ if(i!==''){
+itemsGet();
+const itemsShow= trimmedtodos.map((i,ind)=>{
+   if(i!==''){
     return(
       <li className="item" key={i+ind.toString()}>
         <div className="item-box">{i}</div>
@@ -48,14 +51,12 @@ const itemsShow= props.todos.map((i,ind)=>{ if(i!==''){
 })
 
 
-//const items=localStorage.map(i=>console.log(i))
-
   return (
 
     <div className="todoList">
       <div className="listbox">
       <ul  className="list">
-   {itemsShow}
+            {itemsShow}
             <li className="item"><div className="item-box">Add shit...</div></li>
       </ul>
       </div>
